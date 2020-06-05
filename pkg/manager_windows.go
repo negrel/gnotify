0ae837bbb11c5ea734fd08bd052c9fddabfc1b85
Alexandre Negrel <negrel.dev@protonmail.com>
2020-06-05 22:13:59 +0200

    adding windows notification minimal support

--- pkg/manager_windows.go
+++ pkg/manager_windows.go
@@ -2,9 +2,54 @@
 
 package gnotify
 
+import (
+	"github.com/go-toast/toast"
+)
+
+var _ Manager = &WindowsManager{}
+
+// WindowsManager is the notification manager for Windows system.
 type WindowsManager struct {
+	appName string
 }
 
-func New(Manager, error) {
-	return nil, nil
+// New return a new Notification manager.
+func New(appName string) (Manager, error) {
+	return &WindowsManager{
+		appName: appName,
+	}, nil
+}
+
+/*****************************************************
+ ********************* Methods ***********************
+ *****************************************************/
+// ANCHOR Methods
+
+// Push the given notification to display it.
+func (m *WindowsManager) Push(notif *Notification) error {
+	actions := func() []toast.Action {
+		list := make([]toast.Action, 0, len(notif.Actions))
+
+		for name := range notif.Actions {
+			list = append(list, toast.Action{
+				Type:      "protocol",
+				Label:     name,
+				Arguments: "",
+			})
+		}
+
+		return list
+	}()
+
+	toast := toast.Notification{
+		AppID:   m.appName,
+		Title:   notif.Title,
+		Message: notif.Body,
+		Icon:    notif.Icon,
+		Actions: actions,
+	}
+
+	err := toast.Push()
+
+	return err
 }
